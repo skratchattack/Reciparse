28d5ddd084893471b79892427e593d99026ecd84
Orri <orrmann@gmail.com>
2024-01-02 18:08:28 +0200

    Initial Commit

---
+++ Reciparse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Reciparse
+//
+//  Created by Orri Arnórsson on 2.1.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Reciparse/ReciparseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReciparseApp.swift
+//  Reciparse
+//
+//  Created by Orri Arnórsson on 2.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct ReciparseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
